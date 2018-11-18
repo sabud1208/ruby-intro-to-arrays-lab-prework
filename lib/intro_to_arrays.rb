c35bd6d34fd5bcb44b7e42615963975a8400b7c0
Stephanie Messad <sabud1208@gmail.com>
2018-11-18 17:53:39 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -11,8 +11,6 @@ def array_with_two_elements
 end
 
 
-
-
 def first_element(taylor_swift)
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   puts taylor_swift.shift
